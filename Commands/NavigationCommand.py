e5062360726d076582a9ea440ca7e536d41c066a
Mikov Denis <denis.mikov.06@gmail.com>
2024-04-28 12:21:31 +0300

    implemented go command (navigation)

---
+++ Commands/NavigationCommand.py
@@ -0,0 +1,23 @@
+import os.path
+
+from Commands.Command import Command
+
+
+class NavigationCommand(Command):
+    def _execute(self, args):
+        target = args[0]
+        if target == '..':
+            path = os.path.dirname(self._console.current_directory)
+
+            # Prevent from leaving working directory
+            if path == os.path.dirname(self._console.get_working_directory()):
+                path = self._console.get_working_directory()
+        else:
+            path = os.path.join(self._console.current_directory, target)
+
+        if os.path.exists(path):
+            self._console.current_directory = path
+        else:
+            print(f'{path} does not exist')
+
+
